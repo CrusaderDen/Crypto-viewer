418ef1a6e0f118e6ecea6bc0a10fb2bb64513961
Denis Krestov <rupo@list.ru>
2023-10-31 12:38:22 +0300

    Refactoring Step 1

--- src/App.vue
+++ src/App.vue
@@ -30,12 +30,7 @@
          <section>
             <div class="flex">
                <div class="max-w-lg" style="width: 400px">
-                  <label
-                     for="wallet"
-                     class="block text-sm font-medium text-gray-700"
-                  >
-                     Тикер
-                  </label>
+                  <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
                   <div class="mt-1 relative rounded-md shadow-md">
                      <input
                         @input="autoComplite()"
@@ -46,12 +41,11 @@
                         id="wallet"
                         class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                         placeholder="Например: BTC, ETH, SOL, DOGE, XRP"
+                        autocomplete="off"
                      />
                   </div>
                   <template v-if="tips.length">
-                     <div
-                        class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
-                     >
+                     <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
                         <span
                            v-for="tip in tips"
                            :key="tip.id"
@@ -94,14 +88,14 @@
             <div>
                <button
                   v-if="page > 1"
-                  @click="page = page - 1"
+                  @click="page = +page - 1"
                   class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                >
                   Назад
                </button>
                <button
                   v-if="hasNextPage"
-                  @click="page = page + 1"
+                  @click="page = +page + 1"
                   class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                >
                   Вперед
@@ -113,18 +107,16 @@
             <hr class="w-full border-t border-gray-600 my-4" />
             <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
                <div
-                  v-for="t in filteredTickers()"
+                  v-for="t in paginatedTickers"
                   :key="t.name"
                   @click="select(t)"
                   :class="{
-                     'border-4': sel === t,
+                     'border-4': selectedTicker === t,
                   }"
                   class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
                >
                   <div class="px-4 py-5 sm:p-6 text-center">
-                     <dt class="text-sm font-medium text-gray-500 truncate">
-                        {{ t.name }} - USD
-                     </dt>
+                     <dt class="text-sm font-medium text-gray-500 truncate">{{ t.name }} - USD</dt>
                      <dd class="mt-1 text-3xl font-semibold text-gray-900">
                         {{ t.price }}
                      </dd>
@@ -152,26 +144,20 @@
                </div>
             </dl>
             <hr class="w-full border-t border-gray-600 my-4" />
-            <!-- <div>Страницa: {{ page }}/{{ totalPages }}</div> -->
+            <div>Страницa: {{ page }}/{{ totalPages }}</div>
             <hr class="w-full border-t border-gray-600 my-4" />
          </template>
-         <section v-if="sel" class="relative">
-            <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-               {{ sel.name }} - USD
-            </h3>
+         <section v-if="selectedTicker" class="relative">
+            <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">{{ selectedTicker.name }} - USD</h3>
             <div class="flex items-end border-gray-600 border-b border-l h-64">
                <div
-                  v-for="(bar, idx) in normalizeGraph()"
+                  v-for="(bar, idx) in normalizedGraph"
                   :key="idx"
                   :style="{height: `${bar}%`}"
                   class="bg-purple-800 border w-10"
                ></div>
             </div>
-            <button
-               @click="sel = null"
-               type="button"
-               class="absolute top-0 right-0"
-            >
+            <button @click="selectedTicker = null" type="button" class="absolute top-0 right-0">
                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -206,28 +192,29 @@ export default {
    data() {
       return {
          ticker: "",
-         tickers: [],
-         sel: null,
-         graph: [],
-         showMessage: false,
-         coins: "",
-         tips: [],
-         message: "",
-         page: 1,
-         // totalPages: 1,
          filter: "",
-         hasNextPage: true,
+
+         tickers: [],
+         selectedTicker: null,
+
+         graph: [],
+
+         page: 1,
+
+         tips: [],
+         coins: "",
+         showMessage: false,
+         message: "",
       }
    },
 
    created() {
-      const windowData = Object.fromEntries(
-         new URL(window.location).searchParams.entries()
-      )
+      const windowData = Object.fromEntries(new URL(window.location).searchParams.entries())
+      
       if (windowData.filter) {
          this.filter = windowData.filter
       }
- 
+
       if (windowData.page) {
          this.page = windowData.page
       }
@@ -249,19 +236,49 @@ export default {
       this.coins = coinsJson.Data
    },
 
-   methods: {
-      filteredTickers() {
-         const start = (this.page - 1) * 6
-         const end = this.page * 6
-
-         const filteredTickers = this.tickers.filter((ticker) =>
-            ticker.name.includes(this.filter.toUpperCase())
-         )
-         this.hasNextPage = filteredTickers.length > end
-         // this.totalPages = Math.ceil(filteredTickers.length/6)
-         return filteredTickers.slice(start, end)
+   computed: {
+      startIndex() {
+         return (this.page - 1) * 6
+      },
+      endIndex() {
+         return this.page * 6
       },
 
+      filteredTickers() {
+         return this.tickers.filter((ticker) =>
+            ticker.name.includes(this.filter.toUpperCase())
+         )
+      },
+
+      paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex)
+      },
+
+      totalPages() {
+        return Math.ceil(this.filteredTickers.length / 6)
+      },
+
+      hasNextPage() {
+         return this.filteredTickers.length > this.endIndex
+      },
+
+      normalizedGraph() {
+         const maxValue = Math.max(...this.graph)
+         const minValue = Math.min(...this.graph)
+         if (maxValue === minValue) {
+            return this.graph.map(() => 50)
+         }
+         return this.graph.map((price) => 5 + ((price - minValue) * 95) / (maxValue - minValue))
+      },
+      pageStateOptions(){
+         return {
+            filter: this.filter,
+            page: this.page,
+         }
+      },
+   },
+
+   methods: {
       subscribeToUpdates(tickerName) {
          setInterval(async () => {
             const f = await fetch(
@@ -269,18 +286,18 @@ export default {
             )
             const data = await f.json()
 
-            // this.tickers.find((t) => t.name === tickerName).price =
-            //    data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
+            this.tickers.find((t) => t.name === tickerName).price =
+               data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
 
-            if (this.sel?.name === tickerName) {
+            if (this.selectedTicker?.name === tickerName) {
                this.graph.push(data.USD)
             }
-         }, 10000)
+         }, 30000)
 
          this.ticker = ""
          this.tips = []
       },
-
+     
       add() {
          const currentTicker = {
             name: this.ticker.toUpperCase(),
@@ -300,12 +317,8 @@ export default {
             this.message = "Такого тикера не существует"
             this.showMessage = true
          } else {
-            this.tickers.push(currentTicker)
-
-            localStorage.setItem("crypto-list", JSON.stringify(this.tickers))
-
+            this.tickers = [...this.tickers, currentTicker]
             this.subscribeToUpdates(currentTicker.name)
-
             this.filter = ""
          }
       },
@@ -315,25 +328,18 @@ export default {
       },
 
       select(ticker) {
-         this.sel = ticker
-         this.graph = []
+         this.selectedTicker = ticker
       },
 
       handleDelete(tickerToRemove) {
          this.tickers = this.tickers.filter((t) => t !== tickerToRemove)
-         localStorage.setItem("crypto-list", JSON.stringify(this.tickers))
+         if (this.selectedTicker === tickerToRemove) {
+            this.selectedTicker = null
+         }
       },
 
-      normalizeGraph() {
-         const maxValue = Math.max(...this.graph)
-         const minValue = Math.min(...this.graph)
-         return this.graph.map(
-            (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-         )
-      },
       autoComplite() {
          this.showMessage = false
-         console.clear()
          this.tips = []
          let input = this.ticker.toUpperCase()
          if (input !== "") {
@@ -343,34 +349,36 @@ export default {
                }
                if (this.tips.length > 3) break
             }
-            for (let coin of this.tips) {
-               console.log(coin.Symbol)
-            }
          }
       },
    },
    watch: {
+      selectedTicker(){
+         this.graph = []
+      },
+
+      tickers(){
+         localStorage.setItem("crypto-list", JSON.stringify(this.tickers))
+      },
+
+      paginatedTickers(){
+         if (this.paginatedTickers.length===0 && this.page>1) {
+            this.page -= 1
+         }
+      },
+
       filter() {
          this.page = 1
+      },
+
+      pageStateOptions(v) {
          window.history.pushState(
             null,
             document.title,
-            `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
-         )
-         console.log(
-            Object.fromEntries(new URL(window.location).searchParams.entries())
+            `${window.location.pathname}?filter=${v.filter}&page=${v.page}`
          )
       },
-      page() {
-         window.history.pushState(
-            null,
-            document.title,
-            `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
-         )
-         console.log(
-            Object.fromEntries(new URL(window.location).searchParams.entries())
-         )
-      },
+
    },
 }
 </script>
